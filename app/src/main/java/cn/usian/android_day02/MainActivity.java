7eda6bd42edf32fff7a329a5568f6bca7e2f9eca
lz10hnr <923622583@qq.com>
2017-03-07 13:45:43 +0800

    增加sayHello 这个方法实现了打印输出的功能

--- app/src/main/java/cn/usian/android_day02/MainActivity.java
+++ app/src/main/java/cn/usian/android_day02/MainActivity.java
@@ -2,6 +2,7 @@ package cn.usian.android_day02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sayHello(){
-
+        Log.d("MainActivity", "sayHello");
     }
 }
